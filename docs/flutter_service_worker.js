d8dd318fa0ab7b41885efdd0f81c7675fe31157a
Vagner Willian <tec_vagner.ti@outlook.com>
2022-01-21 06:48:18 -0300

    release render auto

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -57,8 +57,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "82719547f9cb3094316241ed067b06cf",
-"/": "82719547f9cb3094316241ed067b06cf",
+"index.html": "ae1ea2cc7b952157aa4c1072c05afd80",
+"/": "ae1ea2cc7b952157aa4c1072c05afd80",
 "main.dart.js": "d5fc48af80f2cbb31462e9fbefab852f",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
