074d89afa64f43ea0c7f746155b550a291c1b63a
Vagner Willian <tec_vagner.ti@outlook.com>
2022-01-27 18:43:45 -0300

    resolve bug in search callback

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -57,9 +57,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "39c7ed6f1b9412f805ca4d425fec0055",
-"/": "39c7ed6f1b9412f805ca4d425fec0055",
-"main.dart.js": "1ebf3bf1baa228e5ee7c2bd958faffe2",
+"index.html": "76d6f84e298fd2ed59dbe894ee93a096",
+"/": "76d6f84e298fd2ed59dbe894ee93a096",
+"main.dart.js": "2814b7728a783b52fd48321f3c30668b",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
 };
