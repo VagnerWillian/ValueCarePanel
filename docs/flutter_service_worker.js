6e375b327cf00097cd7237f4291a47dc002182cc
Vagner Willian <tec_vagner.ti@outlook.com>
2022-01-27 02:21:19 -0300

    set classification functional

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -57,9 +57,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "44aeeea9340a5f23ce33ae97c2bbbc93",
-"/": "44aeeea9340a5f23ce33ae97c2bbbc93",
-"main.dart.js": "6e7acfb631cd90920db02505cd923411",
+"index.html": "48b94d0551f463f6eb3465837774d997",
+"/": "48b94d0551f463f6eb3465837774d997",
+"main.dart.js": "cc81c4a817c663cbe57483acc5261796",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
 };
