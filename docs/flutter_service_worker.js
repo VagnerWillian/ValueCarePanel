c88a270f7d72d63822002ab91028755e3b780c25
Vagner Willian <tec_vagner.ti@outlook.com>
2022-02-03 15:48:37 -0300

    resolve disabled export report button

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -58,9 +58,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0e629a94f02e4ed33526290f28ac7ccb",
-"/": "0e629a94f02e4ed33526290f28ac7ccb",
-"main.dart.js": "d85304c5a527cf4bb8661284162a29e2",
+"index.html": "3cfb9bfa4d79c9a8d5a2d573cd6c4d8f",
+"/": "3cfb9bfa4d79c9a8d5a2d573cd6c4d8f",
+"main.dart.js": "e5d0eef83d8720747011c6b35ea01c8e",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
 };
--- lib/app/modules/scheduling/ui/scheduling_page.dart
+++ lib/app/modules/scheduling/ui/scheduling_page.dart
@@ -161,7 +161,7 @@ class SchedulingPageState extends State<SchedulingPage> {
           height: 50,
           onPressed: generateReportDoc(),
           gradient: LinearGradient(
-            colors: store.loadingSchedulingItems ? [Colors.grey, Colors.grey] : gradientColors,
+            colors: !store.loadingSchedulingItems ? [Colors.grey, Colors.grey] : gradientColors,
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
