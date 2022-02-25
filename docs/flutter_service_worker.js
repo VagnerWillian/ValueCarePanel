c28669e5faed7e5fbd2850f7bf740b7e12b8a277
Vagner Willian <tec_vagner.ti@outlook.com>
2022-02-25 16:54:42 -0300

    ajustes na tela de monitoramento, historico de sintomas e historico do paciente

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -80,9 +80,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "77c1e08dbb62854aa4dfb6e8c286d801",
-"/": "77c1e08dbb62854aa4dfb6e8c286d801",
-"main.dart.js": "5b79a464dd45b74cc7f15a29622c930e",
+"index.html": "a8f2de1e62518987e302ba628677bede",
+"/": "a8f2de1e62518987e302ba628677bede",
+"main.dart.js": "67d85e0b6ad62c4f473099046b18b047",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
 };
