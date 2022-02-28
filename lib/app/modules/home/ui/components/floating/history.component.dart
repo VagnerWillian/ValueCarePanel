e3824e7f8566ff0c6c55e7490c60cf48d1e15346
Vagner Willian <tec_vagner.ti@outlook.com>
2022-02-28 17:34:36 -0300

    history float

--- lib/app/modules/home/ui/components/floating/history.component.dart
+++ lib/app/modules/home/ui/components/floating/history.component.dart
@@ -1,8 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_network/image_network.dart';
 import 'package:value_panel/app/modules/home/ui/components/floating/history.component_store.dart';
 import 'package:value_panel/app/shared/utils.dart';
 
@@ -65,6 +67,61 @@ class _HistoryFloatingState extends ModularState<HistoryFloating, HistoryFloatin
                     ),
                   ),
                 ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10))
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Column(
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Column(
+                                          children: [
+                                            Text("Vagner Willian", style: GoogleFonts.openSans(fontSize: 12, color: primaryColor, fontWeight: FontWeight.bold)),
+                                            Text("28/02/2022 16:32", style: GoogleFonts.openSans(fontSize: 12, color: Colors.grey)),
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(height: 15),
+                                      Text("Registro de ação sobre a classificação do paciente e organização para procurar atendimento medico da PUCR.",
+                                        style: GoogleFonts.openSans(fontSize: 12, color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: ImageNetwork(
+                                borderRadius: BorderRadius.circular(100),
+                                height: 40,
+                                width: 40,
+                                onLoading: Container(),
+                                imageCache: const CachedNetworkImageProvider("https://uploaddeimagens.com.br/images/003/700/538/original/272146181_6804132162995563_4295433218327981100_n.jpg"),
+                                image: "https://uploaddeimagens.com.br/images/003/700/538/original/272146181_6804132162995563_4295433218327981100_n.jpg",
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             )),
       ),
