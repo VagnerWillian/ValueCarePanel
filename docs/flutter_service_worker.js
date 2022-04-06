85fb0111554c012abda46691f4a55a74600357a5
Vagner Willian <tec_vagner.ti@outlook.com>
2022-04-06 01:43:15 -0300

    chat redesign

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -83,9 +83,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8e686200f41a29442d8fa3c89f53e716",
-"/": "8e686200f41a29442d8fa3c89f53e716",
-"main.dart.js": "ffa7df397fb9cf1b1f98230d2df01990",
+"index.html": "98e5282b594595b6ac644e0b5c8edff9",
+"/": "98e5282b594595b6ac644e0b5c8edff9",
+"main.dart.js": "0f975debee76641ea43e15e48ea5627e",
 "manifest.json": "a6bf733c5ff7cd5775faad23095ee26a",
 "version.json": "03f5412e45d2df3d844a7cbea420b6f4"
 };
